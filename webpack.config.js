c97eb968aca0a87abd0d2aa953862c1730d273d6
Anton Lipilin <antonlipilinwork@gmail.com>
2021-07-19 17:46:04 +0400

    delete path to output dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '/',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'RSS Reader',
